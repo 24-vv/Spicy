f24de814f063a98f201638164a2f52a96fe5fa94
24-vv <braidenm.3213@gmail.com>
2024-09-02 07:57:27 -0700

    v1.1
    Spicy | v1.1:
    
    - Added setting saves.
    
    - Added absic theme changing

--- src/cli.py
+++ src/cli.py
@@ -1,12 +1,34 @@
-import os
+import os, json
 from src.generator import generate_proxies
 from src.validator import validate_proxy, auto_deletion
 from src.store import store_proxies, load_stored_proxies
 
 auto_store = False
+theme_color = "\033[91m"
+success_color = "\033[92m"
+
+def set_theme_color(color):
+    global theme_color
+    theme_color = color
+
+def save_settings():
+    settings = {
+        "auto_store": auto_store,
+        "theme_color": theme_color,
+    }
+    with open("src/settings/settings.json", "w") as f:
+        json.dump(settings, f)
+
+def load_settings():
+    global auto_store, theme_color
+    if os.path.exists("src/settings/settings.json"):
+        with open("src/settings/settings.json", "r") as f:
+            settings = json.load(f)
+            auto_store = settings.get("auto_store", False)
+            theme_color = settings.get("theme_color", "\033[91m")
 
 def display_ascii_art():
-    print("\033[91m")
+    print(theme_color)
     print(r"  _____       _                  |  [ 1 ] Generate Proxies")
     print(r" /  ___|     (_)                 |  [ 2 ] Validate Proxies")
     print(r" \ `--. _ __  _  ___ _   _       |  [ 3 ] Settings")
@@ -14,7 +36,7 @@ def display_ascii_art():
     print(r" /\__/ / |_) | | (__| |_| |      |")
     print(r" \____/| .__/|_|\___|\__, |      |")
     print(r"       | |            __/ |      |")
-    print(r"       |_|           |___/ v1.0  |")
+    print(r"       |_|           |___/ v1.1  |")
     print(r"                                 |  Made with ♥ by .24vv")
     print("\033[0m")
 
@@ -26,83 +48,114 @@ def settings():
     while True:
         clear_console()
         display_ascii_art()
-        print("\033[91m[ SETTINGS ]\033[0m")
+        print(f"{theme_color}[ SETTINGS ]\033[0m")
         print()
-        print("\033[93m [ 1 ] Enable Auto Store\033[0m")
-        print("\033[93m [ 2 ] Disable Auto Store\033[0m")
-        print("\033[93m [ 3 ] Back to Main Menu\033[0m")
+        print(f"{theme_color} [ 1 ] Enable Auto Store\033[0m")
+        print(f"{theme_color} [ 2 ] Disable Auto Store\033[0m")
+        print(f"{theme_color} [ 3 ] Change Theme\033[0m")
+        print(f"{theme_color} [ 4 ] Back to Main Menu\033[0m")
         print()
-        choice = input("\033[91m[ SPICY ]\033[0m > \033[97m").strip()
+        choice = input(f"{theme_color}[ SPICY ]\033[0m > \033[97m").strip()
         print()
         if choice == "1":
             auto_store = True
-            print("\033[92m[ Success ]\033[0m Auto Store enabled")
+            print(f"{theme_color}[ Success ]\033[0m Auto Store enabled")
             print()
-            input("\033[91m[ INFO ]\033[0m Press Enter to continue...")
+            input(f"{theme_color}[ SPICY ]\033[0m Press Enter to continue...")
         elif choice == "2":
             auto_store = False
-            print("\033[92m[ Success ]\033[0m Auto Store disabled")
+            print(f"{theme_color}[ Success ]\033[0m Auto Store disabled")
             print()
-            input("\033[91m[ INFO ]\033[0m Press Enter to continue...")
+            input(f"{theme_color}[ SPICY ]\033[0m Press Enter to continue...")
         elif choice == "3":
+            print(f"{theme_color}[ SPICY ]\033[0m Choose a theme:")
+            print()
+            print(f"{theme_color} [ 1 ] Red\033[0m")
+            print(f"{theme_color} [ 2 ] Purple\033[0m")
+            print(f"{theme_color} [ 3 ] Yellow\033[0m")
+            print(f"{theme_color} [ 4 ] Orange\033[0m")
+            print(f"{theme_color} [ 5 ] Blue\033[0m")
+            print()
+            choice = input(f"{theme_color}[ SPICY ]\033[0m > \033[97m").strip()
+            if choice == "1":
+                set_theme_color("\033[91m")
+            elif choice == "2":
+                set_theme_color("\033[95m")
+            elif choice == "3":
+                set_theme_color("\033[93m")
+            elif choice == "4":
+                set_theme_color("\033[33m")
+            elif choice == "5":
+                set_theme_color("\033[94m")
+            else:
+                print(f"{theme_color}[ ERROR ]\033[0m Unknown theme. Type '1', '2', '3', '4', or '5'.")
+                input(f"{theme_color}[ SPICY ]\033[0m Press Enter to continue...")
+                continue
+            print()
+            print(f"{success_color}[ Success ]\033[0m Theme color changed")
+            print()
+            input(f"{theme_color}[ SPICY ]\033[0m Press Enter to continue...")
+        elif choice == "4":
+            save_settings()
             clear_console()
             display_ascii_art()
             break
         else:
-            print("\033[91m[ ERROR ]\033[0m Unknown command. Type '1', '2', or '3'.")
+            print(f"{theme_color}[ ERROR ]\033[0m Unknown command. Type '1', '2', '3', or '4'.")
             print()
-            input("\033[91m[ INFO ]\033[0m Press Enter to continue...")
+            input(f"{theme_color}[ SPICY ]\033[0m Press Enter to continue...")
 
 def main():
+    load_settings()
     window = "mode 89,13"
     os.system(window)
-    os.system('title "Spicy | v1.0"')
+    os.system('title "Spicy | v1.1"')
 
     display_ascii_art()
 
     while True:
-        command = input("\033[91m[ SPICY ]\033[0m > ").strip().lower().split()
+        command = input(f"{theme_color}[ SPICY ]\033[0m > ").strip().lower().split()
         if command[0] == "1":
-            num = command[1] if len(command) > 1 and command[1].isdigit() else input("\033[91m[ GENERATOR ]\033[0m Enter the number of proxies to generate: \033[97m")
+            num = command[1] if len(command) > 1 and command[1].isdigit() else input(f"{theme_color}[ GENERATOR ]\033[0m Enter the number of proxies to generate: \033[97m")
             proxies = generate_proxies(int(num))
             print()
             if auto_store:
                 store_proxies(proxies)
-                print(f'\033[92m[ Success ]\033[0m Successfully Generated {len(proxies)} proxies')
+                print(f'{theme_color}[ Success ]\033[0m Successfully Generated {len(proxies)} proxies')
             else:
                 for proxy in proxies:
-                    print(f'\033[92m[ Success ]\033[0m Successfully Generated {proxy}')
+                    print(f'{theme_color}[ Success ]\033[0m Successfully Generated {proxy}')
             print()
         elif command[0] == "2":
             if auto_store:
-                choice = input("\033[91m[ VALIDATOR ]\033[0m Validate proxies inside output.txt? (y/n): \033[97m").strip().lower()
+                choice = input(f"{theme_color}[ VALIDATOR ]\033[0m Validate proxies inside output.txt? (y/n): \033[97m").strip().lower()
                 print()
                 if choice == 'y':
                     proxies = load_stored_proxies()
                     for proxy in proxies:
                         is_valid = validate_proxy(proxy)
-                        print(f'\033[91m[ VALIDATOR ]\033[0m Proxy {proxy} is \033[97m{"valid" if is_valid else "invalid"}')
+                        print(f'{theme_color}[ VALIDATOR ]\033[0m Proxy {proxy} is \033[97m{"valid" if is_valid else "invalid"}')
                     auto_deletion()
                 else:
-                    proxy = input("\033[91m[ VALIDATOR ]\033[0m Enter the proxy to validate: \033[97m")
+                    proxy = input(f"{theme_color}[ VALIDATOR ]\033[0m Enter the proxy to validate: \033[97m")
                     is_valid = validate_proxy(proxy)
-                    print(f'\033[91m[ VALIDATOR ]\033[0m Proxy {proxy} is \033[97m{"valid" if is_valid else "invalid"}')
+                    print(f'{theme_color}[ VALIDATOR ]\033[0m Proxy {proxy} is \033[97m{"valid" if is_valid else "invalid"}')
             else:
-                proxy = input("\033[91m[ VALIDATOR ]\033[0m Enter the proxy to validate: \033[97m")
+                proxy = input(f"{theme_color}[ VALIDATOR ]\033[0m Enter the proxy to validate: \033[97m")
                 print()
                 is_valid = validate_proxy(proxy)
-                print(f'\033[91m[ VALIDATOR ]\033[0m Proxy {proxy} is \033[97m{"valid" if is_valid else "invalid"}')
+                print(f'{theme_color}[ VALIDATOR ]\033[0m Proxy {proxy} is \033[97m{"valid" if is_valid else "invalid"}')
                 print()
         elif command[0] == "3":
             settings()
         elif command[0] == "4":
             print()
-            print('\033[91m[ SPICY ]\033[0m Goodbye!')
+            print(f'{theme_color}[ SPICY ]\033[0m Goodbye!')
             break
         else:
             print()
-            print("\033[91m[ ERROR ]\033[0m Unknown command. Type '1', '2', '3', or '4'.")
+            print(f"{theme_color}[ ERROR ]\033[0m Unknown command. Type '1', '2', '3', or '4'.")
             print()
 
-if __name__ == '__': 
+if __name__ == '__main__':
     main()
